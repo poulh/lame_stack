aade3ed907d5fc6378f006fa921b329f3cd9f918
Poul Hornsleth <phornsleth@synapsloans.com>
2018-09-10 14:52:24 -0400

    first commit

---
+++ client/src/main.ts
@@ -0,0 +1,24 @@
+/*
+ * Entry point of the application.
+ * Only platform bootstrapping code should be here.
+ * For app-specific initialization, use `app/app.component.ts`.
+ */
+
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from '@app/app.module';
+import { environment } from '@env/environment';
+
+import { LoopBackConfig } from '../sdk';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
+
+
+
+LoopBackConfig.setBaseURL("http://localhost:3000");
